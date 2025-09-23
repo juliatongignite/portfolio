255e8d402567561e75186c1a1ce59a2481119fd8
Hriday-paul <hridoychandrapaul.10@gmail.com>
2025-09-23 11:17:57 +0600

    remove unneccessary section3,4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import Section1 from "@/components/Pages/Home/Section1/Section1";
 import Section2 from "@/components/Pages/Home/Section2/Section2";
 import Section3 from "@/components/Pages/Home/Section3/Section3";
-import Section4 from "@/components/Pages/Home/Section4/Section4";
-import Section5 from "@/components/Pages/Home/Section5/Section5";
 
 export default function Home() {
   return (
@@ -10,8 +8,7 @@ export default function Home() {
       <Section1 />
       <Section2 />
       <Section3 />
-      {/* <Section4 /> */}
-      {/* <Section5 /> */}
+     
     </div>
   );
 }
